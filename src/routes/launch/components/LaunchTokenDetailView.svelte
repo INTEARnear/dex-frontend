5a5f65d7a42e2527b953b4de2ffd27e69ff7ffaa
Slime <4sliman4@gmail.com>
2026-02-27 18:51:18 +0200

    Minor style changes to iframe

--- src/routes/launch/components/LaunchTokenDetailView.svelte
+++ src/routes/launch/components/LaunchTokenDetailView.svelte
@@ -290,7 +290,8 @@
   .chart-frame {
     width: 100%;
     height: 100%;
-    border: 0;
+    border: 1px solid var(--border-color);
+    border-radius: 0.375rem 1rem 1rem 1rem;
     display: block;
   }
 
